aeb64f61e91029928db79f75a8ed4bec2d5a069f
abdou0dev <136907713+abdou0dev@users.noreply.github.com>
2023-06-23 23:24:33 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const characters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
+    'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3',
+    '4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7',
+    '8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@',
+    '#', '$', '%', '&', '*', '(', ')', '_', '-', '+', '=', '!', '@', '#',
+    '$', '%', '&', '*', '(', ')', '_', '-', '+', '=', '!', '@', '#', '$',
+    '%', '&', '*', '(', ')', '_', '-', '+', '='
+]
+
+let passwordLength = 10
+
+let randomIndex = ""
+let generatedPassword = false
+
+let clickEl = document.getElementById("click-to-copy")
+let passwordEl = document.getElementById("password1-el")
+let password2El = document.getElementById("password2-el")
+let password3El = document.getElementById("password3-el")
+let password4El = document.getElementById("password4-el")
+
+function generate() {
+    generatedPassword = true
+    clickEl.textContent = "Click a password to copy"
+
+    if (randomIndex === "") {
+        for ( let i = 0; i< passwordLength; i++) {
+            randomIndex = Math.floor(Math.random() * characters.length)
+            passwordEl.textContent += characters[randomIndex]
+
+            randomIndex = Math.floor(Math.random() * characters.length)
+            password2El.textContent += characters[randomIndex]
+
+            randomIndex = Math.floor(Math.random() * characters.length)
+            password3El.textContent += characters[randomIndex]
+
+            randomIndex = Math.floor(Math.random() * characters.length)
+            password4El.textContent += characters[randomIndex]
+        }
+
+    }
+
+}
+
+passwordEl.addEventListener("click", function() {
+    if (generatedPassword) {
+        let textToCopy = this.textContent
+        navigator.clipboard.writeText(textToCopy)
+        passwordEl.textContent = "Copied to clipboard"
+
+    }
+})
+password2El.addEventListener("click", function() {
+    if (generatedPassword) {
+        let textToCopy = this.textContent
+        navigator.clipboard.writeText(textToCopy)
+        password2El.textContent = "Copied to clipboard"
+        
+    }
+})
+password3El.addEventListener("click", function() {
+    if (generatedPassword) {
+        let textToCopy = this.textContent
+        navigator.clipboard.writeText(textToCopy)
+        password3El.textContent = "Copied to clipboard"
+        
+    }
+})
+password4El.addEventListener("click", function() {
+    if (generatedPassword) {
+        let textToCopy = this.textContent
+        navigator.clipboard.writeText(textToCopy)
+        password4El.textContent = "Copied to clipboard"
+        
+    }
+})
